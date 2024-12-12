fb71a8483df3a2e7222ad58095e44c91105d2d83
rux_mur <murtacaevaruxanda@bk.ru>
2024-12-12 12:55:50 +0200

    Add an animation for the heart, for selecting favorite trip.

--- lib/shared/heart.dart
+++ lib/shared/heart.dart
@@ -5,16 +5,61 @@ class Heart extends StatefulWidget {
   _HeartState createState() => _HeartState();
 }
 
-class _HeartState extends State<Heart> {
+class _HeartState extends State<Heart> with SingleTickerProviderStateMixin{
+  bool isFavorite = false;
+  late AnimationController _controller;
+  late Animation _colorAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      duration: Duration(milliseconds: 200),
+      vsync: this,
+    );
+
+    _colorAnimation = ColorTween(begin: Colors.grey[400], end: Colors.red).animate(_controller);
+
+    _controller.addListener(() {
+      print(_controller.value);
+      print(_colorAnimation.value);
+    });
+
+    _controller.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        setState(() {
+          isFavorite = true;
+        });
+      }
+      if (status == AnimationStatus.dismissed) {
+        setState(() {
+          isFavorite = false;
+        });
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return IconButton(
-      icon: Icon(
-        Icons.favorite,
-        color: Colors.grey[400],
-        size: 30,
-      ),
-      onPressed: () {},
+    return AnimatedBuilder(
+      animation: _controller,
+      builder: (BuildContext context, child) {
+        return IconButton(
+          icon: Icon(
+            Icons.favorite,
+            color: _colorAnimation.value,
+            size: 30,
+          ),
+          onPressed: () {isFavorite ? _controller.reverse() : _controller.forward();},
+        );
+      },
     );
   }
 }
